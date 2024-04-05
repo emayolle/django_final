9988e319201359809ed1cb8144a4b0d54c0bbabd
Erwan Mayolle <erwan.mayolle.pro@gmail.com>
2024-04-05 21:38:16 +0200

    feat: Create models
    - Created Movie model (title, year, genres)
    - Created Genre model (name)

--- movie_searcher/admin.py
+++ movie_searcher/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from django.db import models
+import movie_searcher
 # Register your models here.
+
+for model in movie_searcher.models.__dict__.values():
+    try:
+        if issubclass(model, models.Model):
+            admin.site.register(model)
+    except TypeError:
+        pass
--- movie_searcher/models.py
+++ movie_searcher/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.IntegerField()
+    genres = models.ManyToManyField(Genre, related_name='movies')
+
+    def __str__(self):
+        return f"{self.title} ({self.year})"
